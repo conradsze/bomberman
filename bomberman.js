bcc4f643ac11044ac52f6ec7232ddfe25e7a6422
Conrad Sze <conradsze@gmail.com>
2015-06-03 14:35:27 -0700

    add picture for glenn

--- bomberman.js
+++ bomberman.js
@@ -1,18 +1,20 @@
 gameStream = new Meteor.Stream('game');
-game = "";
+
 if (Meteor.isClient) {
 
-game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.image('jimmy', 'assets/img/jimmy.png');
+    game.load.image('jimmy', 'assets/img/jimmyball.png');
     game.load.image('glenn', 'assets/img/glenn.png');
     game.load.image('bomb', 'assets/img/bomb.png');
     game.load.image('bg', 'assets/img/bg.jpg');
     game.load.image('box', 'assets/img/box.jpg');
     game.load.image('fire', 'assets/img/fire.png');
     game.load.image('tomb', 'assets/img/rip.png');
+    game.load.image('star', 'assets/img/ninjastar.png');
+
 
 
 }
@@ -21,10 +23,14 @@ var jimmy ="";
 var glenn;
 // var bomb;
 var boxes;
-jimmyLeft=null;
-jimmyRight=null;
-jimmyUp=null;
-jimmyDown=null;
+var jimmyLeft=null;
+var jimmyRight=null;
+var jimmyUp=null;
+var jimmyDown=null;
+var glennLeft=null;
+var glennRight=null;
+var glennUp=null;
+var glennDown=null;
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -41,13 +47,33 @@ function create() {
       
         game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(function(){
         if (jimmy.alive){
-        gameStream.emit('bombSet',{x: jimmy.x, y: jimmy.y});
-        setbomb(jimmy);
+        gameStream.emit('bombSet',{x: jimmy.x, y: jimmy.y,type:"bomb"});
+        setbomb(jimmy, 'bomb');
           }
         });
 
-        var checkposition = this.game.time.events.loop(500, function(){gameStream.emit('jimmyMove', {x: jimmy.x, y: jimmy.y})}, this);
-        checkposition.timer.start();
+        // var checkposition = this.game.time.events.loop(500, function(){gameStream.emit('jimmyMove', {x: jimmy.x, y: jimmy.y})}, this);
+        // checkposition.timer.start();
+
+
+        game.input.keyboard.addKey(Phaser.Keyboard.A).onDown.add(function(){gameStream.emit('glennLeft',  {key: true})});
+        game.input.keyboard.addKey(Phaser.Keyboard.D).onDown.add(function(){gameStream.emit('glennRight',  {key: true})});
+        game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(function(){gameStream.emit('glennUp',  {key: true})});
+        game.input.keyboard.addKey(Phaser.Keyboard.S).onDown.add(function(){gameStream.emit('glennDown',  {key: true})});
+        game.input.keyboard.addKey(Phaser.Keyboard.A).onUp.add(function(){gameStream.emit('glennLeft',  {x: glenn.x, y: glenn.y, key: false})});
+        game.input.keyboard.addKey(Phaser.Keyboard.D).onUp.add(function(){gameStream.emit('glennRight',  {x: glenn.x, y: glenn.y, key: false})});
+        game.input.keyboard.addKey(Phaser.Keyboard.W).onUp.add(function(){gameStream.emit('glennUp',  {x: glenn.x, y: glenn.y, key: false})});
+        game.input.keyboard.addKey(Phaser.Keyboard.S).onUp.add(function(){gameStream.emit('glennDown',  {x: glenn.x, y: glenn.y, key: false})});
+      
+        game.input.keyboard.addKey(Phaser.Keyboard.Z).onDown.add(function(){
+        if (glenn.alive){
+        gameStream.emit('bombSet',{x: glenn.x, y: glenn.y, type:"star"});
+        setbomb(glenn, "star");
+          }
+        });
+
+        // var checkposition = this.game.time.events.loop(500, function(){gameStream.emit('glennMove', {x: glenn.x, y: glenn.y})}, this);
+        // checkposition.timer.start();
 
 
   game.add.tileSprite(0, 0, 900, 636, 'bg');
@@ -72,7 +98,7 @@ function create() {
 
 
     jimmy = game.add.sprite(50, 100, 'jimmy');
-    jimmy.scale.set(0.3);
+    jimmy.scale.set(0.14);
     game.physics.enable(jimmy, Phaser.Physics.ARCADE);
     jimmy.enableBody = true;
     jimmy.body.collideWorldBounds = true;
@@ -128,6 +154,39 @@ function create() {
     };
   })
 
+  gameStream.on('glennLeft', function(data){
+    glennLeft = data.key
+       if (data.x) {
+      glenn.x = data.x;
+      glenn.y = data.y;
+    };
+  })
+
+  gameStream.on('glennRight', function(data){
+   glennRight = data.key
+  
+    if (data.x) {
+      glenn.x = data.x;
+      glenn.y = data.y;
+    };
+  })
+
+  gameStream.on('glennDown', function(data){
+   glennDown = data.key
+      if (data.x) {
+      glenn.x = data.x;
+      glenn.y = data.y;
+    };
+  })
+
+  gameStream.on('glennUp', function(data){
+   glennUp = data.key
+      if (data.x) {
+      glenn.x = data.x;
+      glenn.y = data.y;
+    };
+  })
+
   gameStream.on('jimmyMove', function(data){
     jimmy.x = data.x;
     jimmy.y = data.y;
@@ -139,26 +198,26 @@ function create() {
   })
 
   gameStream.on('bombSet', function(data){
-    setbomb(data);
+    setbomb(data, data.type);
   })
 
 function update(){
 
 
   game.physics.arcade.collide(jimmy, boxes);
-  game.physics.arcade.collide(jimmy, bombs, syncBombs, null, this);
+  game.physics.arcade.collide(jimmy, bombs);
   game.physics.arcade.collide(jimmy, fires, die, null, this);
   game.physics.arcade.collide(boxes, fires, killfire, null, this);
 
   game.physics.arcade.collide(glenn, boxes);
-  game.physics.arcade.collide(glenn, bombs, syncBombs, null, this);
+  game.physics.arcade.collide(glenn, bombs);
   game.physics.arcade.collide(glenn, fires, die, null, this);
 
   jimmy.body.velocity.x = 0;
     jimmy.body.velocity.y = 0;
 
 
-    jimmy.body.setSize(150,125,5,55);
+    jimmy.body.setSize(300,275,0,55);
 
 glenn.body.velocity.x = 0;
     glenn.body.velocity.y = 0;
@@ -191,45 +250,44 @@ glenn.body.velocity.x = 0;
 
 ///////////////////////////////////
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+      
+    if (game.input.keyboard.isDown(Phaser.Keyboard.A) || glennLeft)
     {
         glenn.body.velocity.x = -100;
-        gameStream.emit('glennMove', {x: glenn.x, y: glenn.y});
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+        
+       }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.D) || glennRight)
     {
         glenn.body.velocity.x = 100;
-        gameStream.emit('glennMove', {x: glenn.x, y: glenn.y});
-    }
+        
+       }
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W) || glennUp)
     {
-        glenn.body.velocity.y = -100;
-        gameStream.emit('glennMove', {x: glenn.x, y: glenn.y});
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
+         glenn.body.velocity.y = -100;
+       
+       }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.S) || glennDown)
     {
-        glenn.body.velocity.y = 100;
-        gameStream.emit('glennMove', {x: glenn.x, y: glenn.y});
-    }
+       glenn.body.velocity.y = 100;
+      
+      }
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.Z)) 
-      {
-        if (glenn.alive){
-        gameStream.emit('bombSet',{x: glenn.x, y: glenn.y});
-        setbomb(glenn);
-      }
-      }
 };
 
 
-function setbomb(player){
+function setbomb(player, type){
   var self = this;
   
-      self.bomb = bombs.create(player.x+10, player.y+40, 'bomb');
+      self.bomb = bombs.create(player.x, player.y+45, type);
       bomb.body.collideWorldBounds = true;
+      if(type=="star"){
+        self.bomb.scale.set(0.13);
+      }
+      else{
+        self.bomb.scale.set(0.4);
+      }
       // bomb.body.immovable = true;
-      self.bomb.scale.set(0.4);
       game.time.events.add(4000, explode, self, self.bomb);
 }
 
@@ -243,10 +301,10 @@ function explode(bom){
     fire3.body.velocity.x = -300;
     var fire4 = fires.create(bom.x, bom.y, 'fire');
     fire4.body.velocity.x = 300;
-    fire1.scale.set(0.8);
-    fire2.scale.set(0.8);
-    fire3.scale.set(0.8);
-    fire4.scale.set(0.8);
+    fire1.scale.set(0.7);
+    fire2.scale.set(0.7);
+    fire3.scale.set(0.7);
+    fire4.scale.set(0.7);
     bom.kill();
     console.log("boom!")
 
@@ -262,9 +320,6 @@ function die(player, fire){
     player.kill();
 }
 
-function syncBombs(jimmy, bomb){
-  
-}
 
 
   Template.bomberman.helpers({
